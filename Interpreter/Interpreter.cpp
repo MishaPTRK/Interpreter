82ccac56f9814e8fd2de6763e7300d46bb41f2a8
User <User@WIN-O79RQOTMCOG>
2026-07-15 14:52:40 +0300

    Добавьте файлы проекта.

---
+++ Interpreter/Interpreter.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <vector>
+
+enum class TokenType {NUMBER,IDENT,VAR, DEF,PLUS, MINUS, STAR, SLASH, ASSIGN,
+    LPAREN, RPAREN, LBRACE, RBRACE,COMMA,END_OF_LINE};
+
+
+struct Token {
+    TokenType type;
+    std::string text; 
+    double numValue = 0;
+
+
+class Lexer {
+public:
+    int pos = 0;
+    std::vector<Token> tokens;
+    
+};
+
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
+
