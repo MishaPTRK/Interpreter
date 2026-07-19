608d6b41636044365c62b31d1e7269204153bfb3
Михайло <mishapetruk2019@gmail.com>
2026-07-20 01:41:54 +0300

    parser

--- Interpreter/Parser.h
+++ Interpreter/Parser.h
@@ -71,13 +71,39 @@ public:
 class Parser
 {
 public:
-    std::vector<Token> tokens;
-    int pos;
 
     Parser(std::vector<Token> tokenList)
     {
         tokens = tokenList;
         pos = 0;
+    }
+
+    ExprPtr parseExpr() 
+    {
+        ExprPtr leftSide = parseTerm();
+
+        while (true)
+        {
+            Token currentToken = Peak();
+
+            bool isPlus = (currentToken.type == TokenType::PLUS);
+            bool isMinus = (currentToken.type == TokenType::MINUS);
+
+            if (isPlus == false && isMinus == false)
+            {
+                break;
+            }
+
+            Token operatorToken = advance();
+            char operatorChar = operatorToken.text[0];
+
+            ExprPtr rightSide = parseTerm();
+
+            leftSide = std::make_shared<BinaryExpr>(leftSide, operatorChar, rightSide);
+        }
+
+        return leftSide;
+    
     }
 
 
@@ -98,10 +124,113 @@ private:
     }
 
 
+    Token expect(TokenType exp) 
+    {
+        Token cToken = Peak();
+
+        if (cToken.type != exp) 
+        {
+            throw "Erorr";
+        }
+        return advance();
+    }
+
+    ExprPtr parseTerm() 
+    {
+        ExprPtr lSide = parseFactor();
+
+        while (true) 
+        {
+            Token cToken = Peak();
+
+            if (!(cToken.type == TokenType::STAR) && !(cToken.type == TokenType::SLASH))
+            {
+                break;
+            }
+
+            Token operatorToken = advance();
+            char operatorChar = operatorToken.text[0];
 
 
+            ExprPtr rightSide = parseFactor();
+
+            lSide = std::make_shared<BinaryExpr>(lSide, operatorChar, rightSide);
+        }
+        return lSide;
+    }
 
 
+    ExprPtr parseFactor() 
+    {
+        Token cToken = Peak();
+
+        if (cToken.type == TokenType::NUMBER)
+        {
+            advance();
+            return std::make_shared<numberExpr>(cToken.numValue);
+        }
+
+
+        if (cToken.type == TokenType::LPAREN)
+        {
+            advance();
+            ExprPtr innerExpr = parseExpr();
+            expect(TokenType::RPAREN);
+            return innerExpr;
+        }
+
+        if (cToken.type == TokenType::IDENT)
+        {
+            advance();
+            std::string identifierName = cToken.text;
+
+            Token nextToken = Peak();
+            bool nextIsLparen = (nextToken.type == TokenType::LPAREN);
+
+
+            if (nextIsLparen == true) 
+            {
+                advance();
+
+                std::vector<ExprPtr> argList;
+
+                Token afterLparen = Peak();
+                bool argsAreEmpty = (afterLparen.type == TokenType::RPAREN);
+
+                if (argsAreEmpty == false)
+                {
+                    ExprPtr firstArgument = parseExpr();
+                    argList.push_back(firstArgument);
+
+                    while (true)
+                    {
+                        Token maybeComma = Peak();
+                        bool isComma = (maybeComma.type == TokenType::COMMA);
+
+                        if (isComma == false)
+                        {
+                            break;
+                        }
+
+                        advance();
+
+                        ExprPtr nextArgument = parseExpr();
+                        argList.push_back(nextArgument);
+                    }
+                }
+                expect(TokenType::RPAREN);
+
+                return std::make_shared<CallExpr>(identifierName, argList);
+            
+            }
+            else 
+            {
+                return std::make_shared<variableExpr>(identifierName);
+            }
+        }
+
+
+    }
 
 };
 
