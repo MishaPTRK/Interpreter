3f51e879387ba04751d0648ae74e5e19a7a79d93
Михайло <mishapetruk2019@gmail.com>
2026-07-20 09:33:41 +0300

    Interpreteur

--- Interpreter/Interpreter.cpp
+++ Interpreter/Interpreter.cpp
@@ -5,13 +5,41 @@
 #include "Token.h"
 #include "Lexer.h"
 #include "Parser.h"
-#include "Environment.h'
+#include "Environment.h"
 
 int main()
 {
     Environment env;
     Evaluator evaluator;
+    std::string inputLine;
 
+
+    while (std::getline(std::cin, inputLine)) 
+    {
+        if (inputLine == "exit" || inputLine == "quit")
+        {
+            break;
+        }
+
+        if (inputLine.empty())
+        {
+            continue;
+        }
+
+        Lexer lexer(inputLine);
+        std::vector<Token> tokens = lexer.tokenize();
+
+        Parser parser(tokens);
+        StatementPtr stmt = parser.parseStatement();
+
+        double result = 0;
+
+        if (evaluator.execute(stmt, env, result) == true)
+        {
+            std::cout << result << std::endl;
+        }
+    
+    }
     return 0;
 
 }
